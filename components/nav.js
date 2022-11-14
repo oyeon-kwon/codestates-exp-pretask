7c5abcad9a4a96cc53108c05e3c075b30d981667
oyeon-kwon <rnjsdhdus93@gmail.com>
2022-11-14 23:24:07 +0900

    Add: 네비게이션 바 구현

---
+++ components/nav.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Nav.module.css'
+import Logo from '../public/img/favicon.png'
+import Image from 'next/image';
+
+function Nav() {
+  return <div className={styles.nav}>
+    <div className={styles.nav_leftside}>
+        <Image src={Logo} width={80} height={80} />
+    </div>
+    <div className={styles.nav_rightside}>
+        <div className={styles.nav_link}>수강 중인 코스</div>
+        <div className={styles.nav_link}>코플릿</div>
+        <div className={styles.nav_link}>Jobs</div>
+    </div>
+  </div>
+}
+
+export default Nav
--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+// 컴포넌트 import
+import Nav from '../components/nav';
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>유어클래스 | 코드스테이츠 학습 플랫폼</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/img/favicon.png" />
       </Head>
-
-      <div className={styles.nav}>
-      </div>
+      
+      <Nav />
 
       <main className={styles.main}>
       </main>
